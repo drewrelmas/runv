d22fdcb90c21fba012e4a32b87c3ac6981163939
Drew Relyea <drelyea14@gmail.com>
2025-02-02 16:54:40 -0800

    Working logs and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 mod file_handling;
 mod telemetry;
 
+use file_handling::file_handler::unzip_file;
+use opentelemetry::{global, trace::{Tracer, TracerProvider}};
+use tracing::info;
+
 fn main() {
-    let logger = telemetry::telemetry::init_logs();
-    let meter = telemetry::telemetry::init_metrics();
-    let tracer = telemetry::telemetry::init_tracing();
+    telemetry::telemetry::init_telemetry();
+    do_some_work();
+    telemetry::telemetry::shutdown_telemetry();
+}
+
+fn do_some_work() {
+    let tracer = global::tracer_provider().tracer(telemetry::telemetry::APP_NAME);
+    tracer.in_span("do_some_work", |_cx| {
+        let result = unzip_file("./data/export_28658334.zip", None);
+        if let Err(ref e) = result {
+            info!("Failed to unzip file: {:?}", e);
+        } else {
+            info!("Unzipped file successfully");
+        }
+    });
 }
--- src/telemetry/telemetry.rs
+++ src/telemetry/telemetry.rs
@@ -1,23 +1,51 @@
-use opentelemetry_sdk::metrics::{SdkMeterProvider, PeriodicReader};
-use opentelemetry_sdk::runtime;
+use opentelemetry::{global, KeyValue};
+use opentelemetry_sdk::Resource;
 use opentelemetry_sdk::trace::TracerProvider;
-
 use opentelemetry_sdk::logs::LoggerProvider;
+use opentelemetry_appender_tracing::layer;
+use std::sync::Once;
+use std::sync::Mutex;
+use tracing_subscriber::prelude::*;
 
-pub fn init_tracing() -> TracerProvider {
-    TracerProvider::builder()
+pub const APP_NAME: &str = "runv";
+
+// Since otel currently doesn't have a global logger provider, we need to initialize it once
+static INIT: Once = Once::new();
+static mut LOGGER_PROVIDER: Option<Mutex<LoggerProvider>> = None;
+
+pub fn init_telemetry() {
+    INIT.call_once(|| {
+        init_tracing();
+        let logger_provider = init_logging();
+        unsafe {
+            LOGGER_PROVIDER = Some(Mutex::new(logger_provider));
+        }
+    });
+}
+
+pub fn shutdown_telemetry() {
+    global::shutdown_tracer_provider();
+    unsafe {
+        if let Some(ref provider) = LOGGER_PROVIDER {
+            let _ = provider.lock().unwrap().shutdown();
+        }
+    }
+}
+
+fn init_tracing() {
+    let provider = TracerProvider::builder()
+        .with_resource(Resource::new_with_defaults(vec![KeyValue::new("service.name", APP_NAME)]))
         .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
-        .build()
+        .build();
+    global::set_tracer_provider(provider);
 }
 
-pub fn init_logs() -> LoggerProvider {
-    LoggerProvider::builder()
+fn init_logging() -> LoggerProvider {
+    let provider = LoggerProvider::builder()
+        .with_resource(Resource::new_with_defaults(vec![KeyValue::new("service.name", APP_NAME)]))
         .with_simple_exporter(opentelemetry_stdout::LogExporter::default())
-        .build()
-}
-
-pub fn init_metrics() -> SdkMeterProvider {
-    let exporter = opentelemetry_stdout::MetricExporter::default();
-    let reader = PeriodicReader::builder(exporter, runtime::Tokio).build();
-    SdkMeterProvider::builder().with_reader(reader).build()
+        .build();
+    let layer = layer::OpenTelemetryTracingBridge::new(&provider);
+    tracing_subscriber::registry().with(layer).init();
+    provider
 }
