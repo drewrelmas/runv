1f758d956dd9ea509683c2f02c7fade846a6ea6d
Drew Relyea <drelyea14@gmail.com>
2025-02-15 08:26:32 -0800

    Upgrade to 0.28, still experiencing stackoverflow on run

--- src/telemetry/mod.rs
+++ src/telemetry/mod.rs
@@ -1,1 +1,64 @@
-pub mod telemetry;
+use once_cell::sync::Lazy;
+use opentelemetry::global;
+use opentelemetry_stdout::{SpanExporter, LogExporter, MetricExporter};
+
+// tracing
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::trace::SdkTracerProvider;
+
+// logging
+use opentelemetry_sdk::logs::SdkLoggerProvider;
+use opentelemetry_appender_tracing::layer;
+use tracing_subscriber::prelude::*;
+
+// metrics
+use opentelemetry_sdk::metrics::SdkMeterProvider;
+
+pub const APP_NAME: &str = "runv";
+
+static RESOURCE: Lazy<Resource> = Lazy::new(|| {
+    Resource::builder()
+        .with_service_name(APP_NAME)
+        .build()
+});
+
+pub fn init_telemetry() -> (SdkTracerProvider, SdkLoggerProvider, SdkMeterProvider) {
+    let tracer_provider = init_tracing();
+    let logger_provider = init_logging();
+    let meter_provider = init_metrics();
+    (tracer_provider, logger_provider, meter_provider)
+}
+
+pub fn shutdown_telemetry(providers: (SdkTracerProvider, SdkLoggerProvider, SdkMeterProvider)) {
+    let _ = providers.0.shutdown();
+    let _ = providers.1.shutdown();
+    let _ = providers.2.shutdown();
+}
+
+fn init_tracing() -> SdkTracerProvider {
+    let provider = SdkTracerProvider::builder()
+        .with_resource(RESOURCE.clone())
+        .with_simple_exporter(SpanExporter::default())
+        .build();
+    global::set_tracer_provider(provider.clone());
+    provider
+}
+
+fn init_logging() -> SdkLoggerProvider {
+    let provider = SdkLoggerProvider::builder()
+        .with_resource(RESOURCE.clone())
+        .with_simple_exporter(LogExporter::default())
+        .build();
+    let layer = layer::OpenTelemetryTracingBridge::new(&provider);
+    tracing_subscriber::registry().with(layer).init();
+    provider
+}
+
+fn init_metrics() -> SdkMeterProvider {
+    let provider = SdkMeterProvider::builder()
+        .with_periodic_exporter(MetricExporter::default())
+        .with_resource(RESOURCE.clone())
+        .build();
+    global::set_meter_provider(provider.clone());
+    provider
+}
