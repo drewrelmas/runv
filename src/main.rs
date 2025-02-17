2ddb93724653a2f568decfd89c6105fd405f46e3
Drew Relyea <drelyea14@gmail.com>
2025-02-17 08:43:01 -0800

    Try egui dependency

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 mod file_handling;
 mod telemetry;
+mod app;
 
 use file_handling::file_handler;
 use opentelemetry::{global, trace::{Tracer, TracerProvider}};
 use tracing::info;
+use crate::app::TemplateApp;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> eframe::Result {
     let providers = telemetry::init_telemetry();
-    do_some_work();
-    telemetry::shutdown_telemetry(providers);
-    Ok(())
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        telemetry::APP_NAME,
+        native_options,
+        Box::new(|cc| Ok(Box::new(TemplateApp::new(cc)))),
+    )
+    
+    // do_some_work();
+    // telemetry::shutdown_telemetry(providers);
+    // Ok(())
 }
 
 fn do_some_work() {
